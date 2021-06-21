1024c217664d47b5c79afe4625260ef3f1f466cb
taegyuy <tgyyun@gmail.com>
2021-06-22 02:26:49 +1000

    added delete todo functionality

--- server/index.js
+++ server/index.js
@@ -67,6 +67,20 @@ app.put("/todos/:id", async (req, res) => {
   }
 });
 
+// delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE from todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("todo was deleted");
+  } catch (err) {
+    console.error(err);
+  }
+});
+
 app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
