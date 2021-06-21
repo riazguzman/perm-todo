adad7720ec6d0738ec4178d9deb140ae2362eafc
taegyuy <tgyyun@gmail.com>
2021-06-22 02:22:34 +1000

    added update, get all, get one todos endpoints

--- server/index.js
+++ server/index.js
@@ -19,11 +19,54 @@ app.post("/todos", async (req, res) => {
       [description]
     );
 
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
+
+// get all todos
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allToDos = await pool.query("SELECT * FROM todo");
+    res.json(allToDos.rows);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// get a todo
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.send(todo.rows);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// update a todo
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("todo was updated!");
+  } catch (err) {
+    console.error(err);
+  }
+});
+
 app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
